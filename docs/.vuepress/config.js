37eaef50aad14655c245456b345478e4e9b44448
yonghao.hu <yonghao.hu@daocloud.io>
2020-10-14 15:32:47 +0800

    modified: change file dir

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,36 @@
 module.exports = {
   title: 'Hello VuePress',
   description: 'Just playing around',
-  dest: './dist'
+  themeConfig: {
+    displayAllHeaders: true,
+    sidebar: {
+      '/frontend/': [
+        {
+          title: 'frontend',   // 必要的
+          collapsable: false, // 可选的, 默认值是 true,
+          sidebarDepth: 2,    // 可选的, 默认值是 1
+          children: [
+            'vue-docs',
+            'essays',
+            'interview'
+          ]
+        }
+      ],
+      '/backend/': [
+        {
+          title: 'backend',   // 必要的
+          collapsable: false, // 可选的, 默认值是 true,
+          sidebarDepth: 2,
+          children: [
+            'cs'
+          ]
+        }
+      ]
+    },
+    nav: [{
+      text: '前端', link: '/frontend/'
+    }, {
+      text: '后端', link: '/backend/cs'
+    }]
+  }
 }
--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,8 @@
-<h2> 大H的随笔博客</h2> 
-
-## 1.写在前面
-  自毕业后深刻到记录的重要性，上次遇到同一个错但中间间隔几个月想破头也忘了当时的思路和想法，于是扒拉出之前练手的一个小玩意(就是现在这个)，用于记录问题和思路，偶尔也可能会写些随笔
+---
+home: true
+heroText: EssaysBook
+tagline: 人生苦短，少写代码, 多喝岩浆
+actionText: Enter ->
+actionLink: /frontend/vue-docs/
+---
+# <center> 摸鱼指南 </center>
